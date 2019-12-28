c1182116579f2491d9c5c969c5f545a8f78b87e4
David Mesquita-Morris <mesmo@steelbreeze.net>
2019-12-28 10:10:08 +0000

    Remove d3 from the test page as well

--- src/getHTML.ts
+++ src/getHTML.ts
@@ -1,5 +1,13 @@
 import { Cell } from './Cell';
 
 export function getHTML(table: Array<Array<Cell>>): string {
-    return table.reduce((res: string, row: Array<Cell>) => `${res}<tr>${row.reduce((res: string, cell: Cell) => `${res}<td class="${cell.style} height${Math.round(cell.height / 10)}" colspan="${cell.colspan}" rowspan="${cell.rowspan}">${cell.detail.name || ""}</td>`, "")}</tr>`, "");
+    return table.reduce(rowsBuilder, "");
+}
+
+function rowsBuilder(res: string, row: Array<Cell>): string {
+    return `${res}<tr>${row.reduce(cellsBuilder, "")}</tr>`;
+}
+
+function cellsBuilder(res: string, cell: Cell): string {
+    return `${res}<td class="${cell.style} height${Math.round(cell.height / 10)}" colspan="${cell.colspan}" rowspan="${cell.rowspan}">${cell.detail.name || ""}</td>`;
 }
