62b97119188b3d58ca6769ddeb74922f7468da26
David Mesquita-Morris <mesmo@steelbreeze.net>
2019-12-29 08:06:39 +0000

    Minor change to getAxis

--- src/getAxis.ts
+++ src/getAxis.ts
@@ -5,21 +5,21 @@ import { Dimension } from './Dimension';
 /**
  * Returns the unique set of values within an application data set for a given dimension.
  * @param applications The applications data.
- * @param dimension The dimension to use as the y axis.
+ * @param name The name of the dimension.
  * @returns Returns the axis with its values.
  */
-export function getAxis(applications: Array<Application>, dimension: Dimension): Axis {
-    const result: Axis = { name: dimension, values: [] };
+export function getAxis(applications: Array<Application>, name: Dimension): Axis {
+    const values: Array<string> = [];
 
     for (const app of applications) {
         for (const use of app.usage) {
-            const value = use[dimension];
+            const value = use[name];
 
-            if (result.values.indexOf(value) === -1) {
-                result.values.push(value);
+            if (values.indexOf(value) === -1) {
+                values.push(value);
             }
         }
     }
 
-    return result;
+    return { name, values };
 }
