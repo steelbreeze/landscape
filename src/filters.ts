bc794ca7f2cf027c8c0fbf49b4a1c20c7bae92f6
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-01-14 18:26:55 +0000

    Trim docs a little more; remove double dist directory

--- src/filters.ts
+++ src/filters.ts
@@ -2,6 +2,12 @@ import { Application } from './Application';
 import { Use } from './Use';
 
 /**
+ * An always true predicate.
+ * @hidden
+ */
+const fTrue = () => true;
+
+/**
  * Determine if a date exists within a date range. 
  * @param date The date to test.
  * @param start The start of the range; if undefined the range start at the begining of time.
@@ -20,12 +26,13 @@ function between(date: Date, start: Date | undefined, end: Date | undefined) {
  * @param appCriteria The application criteria
  * @returns The filtered set of applications
  */
-export function filter(applications: Array<Application>, useCriteria: ((use: Use) => boolean) | undefined = undefined, appCriteria: ((app: Application) => boolean) | undefined = undefined): Array<Application> {
-	return applications.filter(appCriteria || (() => true)).map(app => { return { detail: app.detail, usage: app.usage.filter(useCriteria || (() => true)) } }).filter(app => app.usage.length > 0);
+export function filter(applications: Array<Application>, useCriteria: ((use: Use) => boolean) | undefined = fTrue, appCriteria: ((app: Application) => boolean) | undefined = fTrue): Array<Application> {
+	return applications.filter(appCriteria).map(app => { return { detail: app.detail, usage: app.usage.filter(useCriteria) } }).filter(app => app.usage.length > 0);
 }
 
 /**
  * Filters the applications to determine what was in use at a particular point in time.
+ * @remarks The filter uses the optional commissioned and decomissioned date of the application use records.
  * @param applications The full set of applications.
  * @param date The date to filter the applications by.
  * @returns Returns the subset of applications that were in use on the specified date.
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   entry: './lib/index.js',
   mode: 'production',
   output: {
+	path: path.resolve(__dirname, "docs/dist"),
     filename: 'landscape.min.js',
     library: 'landscape',
     libraryTarget: 'var'
