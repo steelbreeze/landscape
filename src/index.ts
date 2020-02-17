6e0e84296cff99df1d090e6faf2d19bbc90914aa
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-02-17 21:01:50 +0000

    Remove filters and legacy file in /lib

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { Axis } from './Axis';
 export { getOptimalAxes, flexOrder } from './getOptimalAxes';
 export { getTable } from './getTable';
 export { getHTML } from './getHTML';
-export { filterByDate } from './filters';
