c03a13cab7b45ebea0540a465504bc78acddedc9
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-02-29 07:56:16 +0000

    Merge getAdjacency into getOptimalAxis

--- src/getOptimalAxes.ts
+++ src/getOptimalAxes.ts
@@ -1,8 +1,17 @@
 import { IApplication } from './IApplication';
+import { IDetail } from './IDetail';
 import { IAxis } from './IAxis';
 import { IAxes } from './IAxes';
-import { FlatApp } from './FlatApp';
-import { getAdjacency } from './getAdjacency';
+
+/**
+ * A denormalised representation of application data.
+ * @hidden
+ */
+interface FlatApp {
+	detail: IDetail;
+	status: string;
+	usage: Array<{ x: string, y: string }>
+}
 
 /**
  * Determine the optimum order of the axes resulting in a layout with applications grouped together
@@ -102,4 +111,42 @@ function flatten(applications: Array<IApplication>, x: string, y: string): Array
 
 	// delete single use app/status combinations as they cannot contribute to affinity score
 	return interim.filter(app => app.usage.length > 1);
+}
+
+
+/**
+ * Gets an adjacencey count for applications in a given scenario (sequence of axes values)
+ * @param denormalised The denormalised application data
+ * @param xAxis The xAxis
+ * @param yAxis The yAxis
+ * @param countX Count adjacency on the x axis
+ * @param countY Count adjacency on the y axis
+ * @returns Returns an adjacency count; the number of adjacent cells in the x and y dimensions
+ * @hidden
+ */
+function getAdjacency(denormalised: Array<FlatApp>, xAxis: Array<string>, yAxis: Array<string>, countX: boolean = true, countY: boolean = true): number {
+	let adjacency = 0;
+
+	// test each application/status combination individually
+	for (const app of denormalised) {
+		// create 2d boolean matrix where the application exists 
+		const matrix = yAxis.map(y => xAxis.map(x => app.usage.some(use => use.y === y && use.x === x)));
+
+		// count adjacent cells
+		for (let iY = yAxis.length; iY--;) {
+			for (let iX = xAxis.length; iX--;) {
+				if (matrix[iY][iX]) {
+					if (countY && iY && matrix[iY - 1][iX]) {
+						adjacency++;
+					}
+
+					if (countX && iX && matrix[iY][iX - 1]) {
+						adjacency++;
+					}
+				}
+			}
+		}
+	}
+
+	return adjacency;
 }
