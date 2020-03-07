91abe6387dfd16339e4b6ce925f5762b7c1d01c6
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-03-07 09:17:55 +0000

    Tidy prepareData

--- src/prepareData.ts
+++ src/prepareData.ts
@@ -3,10 +3,11 @@ import { IApplicationInContext } from './IApplicationInContext';
 import { IAxis } from './IAxis';
 
 /**
- * Denormalises the application data for a given x and y axis.
+ * Structures and denormalises the application aligned to a chosen pair of axes.
  * @param applications The application data to prepare.
- * @param x The x axis dimension to use.
- * @param y The y axis dimension to use.
+ * @param x The chosen x axis.
+ * @param y The chosen y axis.
+ * @returns Returns a 2D array representing the chosen axis; each cell containing an array of the applications used in that context.
  */
 export function prepareData(applications: Array<IApplication>, x: IAxis, y: IAxis): Array<Array<Array<IApplicationInContext>>> {
 	return y.values.map((yValue) => {
@@ -28,3 +29,4 @@ export function prepareData(applications: Array<IApplication>, x: IAxis, y: IAxi
 		});
 	});
 }
+
