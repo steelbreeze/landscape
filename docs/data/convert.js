b647b7f81784c8b3938eaba125484c153bc8166e
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-08-18 22:22:32 +0100

    Updated (corrected) data

--- docs/data/convert.js
+++ docs/data/convert.js
@@ -5,7 +5,7 @@ let csv = 'id,name,capability,product,location,commissioned,decommissioned,statu
 
 for(const application of applications) {
 	for( const use of application.usage) {
-		csv += `\n${application.detail.id},${application.detail.name},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned):undefined},${use.decommissioned?new Date(use.decommissioned):undefined},${use.status}`;
+		csv += `\r\n${application.detail.id},${application.detail.name},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned):''},${use.decommissioned?new Date(use.decommissioned):''},${use.status}`;
 	}
 }
 
