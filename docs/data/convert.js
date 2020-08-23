e51224ded0e9cac58df36e8e77d984dcf85cb2a3
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-08-23 08:00:56 +0100

    Convert using latest cdb

--- docs/data/convert.js
+++ docs/data/convert.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const applications = JSON.parse(fs.readFileSync('./mythicalBank.json').toString());
 let csv = 'id,name,capability,product,location,commissioned,decommissioned,status';
 
-for(const application of applications) {
-	for( const use of application.usage) {
-		csv += `\r\n${application.detail.id},${application.detail.name},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned):''},${use.decommissioned?new Date(use.decommissioned):''},${use.status}`;
+for (const application of applications) {
+	for (const use of application.usage) {
+		csv += `\r\n${application.detail.id},${application.detail.name},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned) : ''},${use.decommissioned ? new Date(use.decommissioned) : ''},${use.status}`;
 	}
 }
 
