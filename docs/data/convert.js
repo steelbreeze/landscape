2fcf57749ed88530e93ebc69912e1880896d0290
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-09-29 08:06:19 +0100

    Update tilly data

--- docs/data/convert.js
+++ docs/data/convert.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 const applications = JSON.parse(fs.readFileSync('./mythicalBank.json').toString());
-let csv = 'id,name,capability,product,location,commissioned,decommissioned,status';
+let csv = 'name,supplier,capability,product,location,commissioned,decommissioned,status';
 
 for (const application of applications) {
 	for (const use of application.usage) {
-		csv += `\r\n${application.detail.id},${application.detail.name},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned) : ''},${use.decommissioned ? new Date(use.decommissioned) : ''},${use.status}`;
+		csv += `\r\n${application.detail.name},${application.detail.supplier},${use.dimensions.capability},${use.dimensions.product},${use.dimensions.location},${use.commissioned ? new Date(use.commissioned) : ''},${use.decommissioned ? new Date(use.decommissioned) : ''},${use.status}`;
 	}
 }
 
