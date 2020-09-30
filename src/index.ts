d0deb3461ee3756892f33a0832471a5bb5665c4e
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-09-30 07:17:09 +0100

    Remove deprecated deriveAxis function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { deriveAxes } from './deriveAxis';
 export { deriveDimensions } from './deriveDimensions';
 export { getOptimalAxes, flexOrder } from './getOptimalAxes';
 export * from './render';
