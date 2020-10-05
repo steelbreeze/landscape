e0cea530bb3fa7ed219b5033f1f3db8e1e206194
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-10-05 20:38:26 +0100

    Use a user-defined key to put unique values in each cell

--- src/prepareData.ts
+++ src/prepareData.ts
@@ -1,4 +1,4 @@
-import { IApplication, IUsage, IUseDetail, IDimensions} from './IApplication';
+import { IApplication, IUsage, IUseDetail, IDimensions, IDetail } from './IApplication';
 import { IAxis } from './IAxis';
 
 /**
@@ -6,23 +6,24 @@ import { IAxis } from './IAxis';
  * @param applications The application data to prepare.
  * @param x The chosen x axis.
  * @param y The chosen y axis.
- * @param key The key to use for uniqueness checking
+ * @param getKey A callback to create a unique key for the reduction of applications into the cells.
  * @returns Returns a 2D array representing the chosen axis; each cell containing an array of the applications used in that context.
  */
-export function prepareData(applications: Array<IApplication & IUsage>, x: IAxis, y: IAxis, key: string = "name"): Array<Array<Array<IApplication & IDimensions & IUseDetail>>> {
-	// create the empty destination table structure
-	const result: Array<Array<Array<IApplication & IDimensions & IUseDetail>>> = y.values.map(() => x.values.map(() => []));
+export function prepareData(applications: Array<IApplication & IUsage>, x: IAxis, y: IAxis, getKey: (detail: IDetail, use: IDimensions & IUseDetail ) => string): Array<Array<Array<IApplication & IDimensions & IUseDetail>>> {	// create the empty destination table structure
+	const result: Array<Array<Array<{ key: string } & IApplication & IDimensions & IUseDetail>>> = y.values.map(() => x.values.map(() => []));
 
 	// denormalise and position each application within the correct table cell
 	for (const app of applications) {
 		for (const use of app.usage) {
+			const key = getKey(app.detail, use);
 			const yIndex = y.values.indexOf(use.dimensions[y.name]);
 			const xIndex = x.values.indexOf(use.dimensions[x.name]);
 
-			// only add the app / use combination if there is a cell in the target table and the app/status combination is unique within that cell
-			if (yIndex !== -1 && xIndex !== -1 && !result[yIndex][xIndex].some(da => da.detail[key] === app.detail[key] && da.status === use.status)) {
-				result[yIndex][xIndex].push({ detail: app.detail, dimensions: use.dimensions, commissioned: use.commissioned, decommissioned: use.decommissioned, status: use.status });
+			// only add the app / use combination if there is a cell if the key is not already there
+			if (yIndex !== -1 && xIndex !== -1 && !result[yIndex][xIndex].some(da => da.key === key)) {
+				result[yIndex][xIndex].push({ key, detail: app.detail, dimensions: use.dimensions, commissioned: use.commissioned, decommissioned: use.decommissioned, status: use.status });
 			}
+			// TODO: merge usage rather than ignore it
 		}
 	}
 
