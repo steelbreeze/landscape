cc856d0be07091177e60e8a3e0039dc9dc30d041
David Mesquita-Morris <mesmo@steelbreeze.net>
2021-08-05 16:59:40 +0100

    Extract common code from splitX and splitY

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,92 @@
 // @steelbreeze/landscape
-// Copyright (c) 2021 David Mesquita-Morris
-import { Axis, Cube, Func1, Row } from '@steelbreeze/pivot';
+// Copyright (c) 2019-21 David Mesquita-Morris
+import { Axis, Cube, Func1, Row, Table } from '@steelbreeze/pivot';
 
-/** The final text and class name to use when rendering cells in a table. */
+/**
+ * The final text and class name to use when rendering cells in a table.
+ */
 export interface Key {
+	/** The text to use in the final table rendering. */
 	text: string;
+
+	/** The class name to use in the final table rendering. */
 	className: string;
 }
 
+/**
+ * An extension of key, adding the number of rows and columns the key will occupy in the final table rendering.
+ */
 export interface Cell extends Key {
+	/** The number of rows to occupy. */
 	rowSpan: number;
+
+	/** The number of columns to occupy. */
 	colSpan: number;
 }
 
 /**
- * Converts a pivoted cube into a table structure, including axes turning cube cells with multiple values into columns.
- * @param cube The source cube
- * @param yAxis The yA
- * @param xAxis 
- * @param getKey 
- * @returns 
+ * Converts a pivoted cube and its axes into a table structure. Where a cell in the cube contains multiple values, multiple columns will be generated.
+ * @param cube The source cube.
+ * @param yAxis The y axis.
+ * @param xAxis The x axis.
+ * @param getKey A callback to extract the Key from the source data.
  */
 export function splitX<TRow extends Row>(cube: Cube<TRow>, yAxis: Axis<TRow>, xAxis: Axis<TRow>, getKey: Func1<TRow, Key>): Cell[][] {
-	const splits = generate(xAxis.length, index => cube.map(row => row[index].length || 1).reduce(leastCommonMultiple));
+	const xSplits = generate(xAxis.length, index => cube.map(row => row[index].length || 1).reduce(leastCommonMultiple));
 
-	const header = generate(xAxis[0].pairs.length, row => [...generate(yAxis[0].pairs.length, () => xyAxisCell), ...xAxis.reduce<Cell[]>((res, c, index) => [...res, ...generate(splits[index], () => cell({ className: `axis x ${c.pairs[row].key}`, text: c.pairs[row].value }))], [])]);
-	const mapped = cube.map((row, ri) => [...yAxis[ri].pairs.map((pair) => cell({ className: `axis y ${pair.key}`, text: pair.value })), ...row.reduce<Cell[]>((res, c, index) => [...res, ...generate(splits[index], nri => cell(c.length ? getKey(c[Math.floor(nri / splits[index] * c.length)]) : emptyKey))], [])]);
+	const header = generate(xAxis[0].pairs.length, row => [...xyHeaderRow(yAxis), ...xAxis.reduce<Cell[]>((res, c, index) => [...res, ...generate(xSplits[index], () => cell({ className: `axis x ${c.pairs[row].key}`, text: c.pairs[row].value }))], [])]);
+	const mapped = cube.map((row, ri) => [...yHeaderRow(yAxis, ri), ...row.reduce<Cell[]>((res, c, index) => [...res, ...generate(xSplits[index], nri => dataCell(c, nri / xSplits[index], getKey))], [])]);
 
 	return [...header, ...mapped];
 }
 
+/**
+ * Converts a pivoted cube and its axes into a table structure. Where a cell in the cube contains multiple values, multiple rows will be generated.
+ * @param cube The source cube.
+ * @param yAxis The y axis.
+ * @param xAxis The x axis.
+ * @param getKey A callback to extract the Key from the source data.
+ */
 export function splitY<TRow extends Row>(cube: Cube<TRow>, yAxis: Axis<TRow>, xAxis: Axis<TRow>, getKey: Func1<TRow, Key>): Cell[][] {
-	const splits = cube.map(row => row.map(cell => cell.length || 1).reduce(leastCommonMultiple));
+	const ySplits = cube.map(row => row.map(cell => cell.length || 1).reduce(leastCommonMultiple));
 
-	const header = generate(xAxis[0].pairs.length, row => [...generate(yAxis[0].pairs.length, () => xyAxisCell), ...xAxis.map(s => cell({ className: `axis x ${s.pairs[row].key}`, text: s.pairs[row].value }))]);
-	const mapped = cube.reduce<Cell[][]>((res, row, index) => [...res, ...generate(splits[index], nri => [...yAxis[index].pairs.map(pair => cell({ className: `axis y ${pair.key}`, text: pair.value })), ...row.map(rec => cell(rec.length ? getKey(rec[Math.floor(rec.length * nri / splits[index])]) : emptyKey))])], []);
+	const header = generate(xAxis[0].pairs.length, row => [...xyHeaderRow(yAxis), ...xAxis.map(s => cell({ className: `axis x ${s.pairs[row].key}`, text: s.pairs[row].value }))]);
+	const mapped = cube.reduce<Cell[][]>((res, row, index) => [...res, ...generate(ySplits[index], nri => [...yHeaderRow(yAxis, index), ...row.map(c => dataCell(c, nri / ySplits[index], getKey))])], []);
 
 	return [...header, ...mapped];
 }
 
+/**
+ * Creates a single row of the xy header block.
+ * @hidden
+ */
+function xyHeaderRow<TRow extends Row>(yAxis: Axis<TRow>): Cell[] {
+	return yAxis[0].pairs.map(() => cell({ className: 'axis xy', text: '' }));
+}
+
+/**
+ * Creates the y axis header section of a row
+ * @hidden 
+ */
+function yHeaderRow<TRow extends Row>(yAxis: Axis<TRow>, row: number): Cell[] {
+	return yAxis[row].pairs.map(pair => cell({ className: `axis y ${pair.key}`, text: pair.value }));
+}
+
+/**
+ * Creates a data cell.
+ * @hidden
+ */
+function dataCell<TRow extends Row>(table: Table<Row>, factor: number, getKey: Func1<TRow, Key>): Cell {
+	return cell(table.length ? getKey(table[Math.floor(table.length * factor)]) : { className: 'empty', text: '' });
+}
+
+/**
+ * Creates a cell within a table
+ * @hidden
+ */
+ function cell(key: Key): Cell {
+	return { ...key, rowSpan: 1, colSpan: 1 };
+}
 /**
  * Merge adjacent cells in a split table on the y and/or x axes.
  * @param table A table of Cells created by a previous call to splitX or splitY.
@@ -94,15 +142,3 @@ function generate<TResult>(size: number, f: (index: number) => TResult): Array<T
 
 	return result;
 }
-
-/**
- * Creates a cell within a table
- * @hidden
- */
- function cell(key: Key): Cell {
-	return { ...key, rowSpan: 1, colSpan: 1 };
-}
-
-// constants used in a variety of places
-const xyAxisCell = cell({ className: 'axis xy', text: '' });
-const emptyKey = { className: 'empty', text: '' };
