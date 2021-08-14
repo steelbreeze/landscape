3a995bb611f7c730de26aa0ad04a5ca4fdbe8e70
David Mesquita-Morris <mesmo@steelbreeze.net>
2021-08-14 08:43:20 +0100

    Comments

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,20 @@ export interface Cell extends Key {
 	colSpan: number;
 }
 
-export function split<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>, yAxis: Dimension<TRow>, getKey: Func1<TRow, Key>, onX: boolean): Cell[][] {
+/**
+ * Generates a table from a cube and its axis.
+ * @param cube The source cube.
+ * @param xAxis The x axis.
+ * @param yAxis The y axis.
+ * @param getKey A callback to generate a key containing the text and className used in the table from the source records,
+ * @param onX A flag to indicate if cells in cube containing multiple values should be split on the x axis (if not, the y axis will be used).
+ */
+export function table<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>, yAxis: Dimension<TRow>, getKey: Func1<TRow, Key>, onX: boolean): Cell[][] {
+	// for each row and column, determine how many sub rows and columns we need to split it into; this is the LCM of the counts of items in that row or column
 	const xSplits = generate(xAxis.length, index => onX ? cube.map(row => row[index].length || 1).reduce(leastCommonMultiple) : 1);
 	const ySplits = cube.map(row => row.map(cell => onX ? 1 : cell.length || 1).reduce(leastCommonMultiple));
 
+	// expand the cube based on the splits and add in the row and column headings
 	return ySplits.reduce<Cell[][]>((result, ySplit, yIndex) => [...result, ...generate(ySplit, nyi => xSplits.reduce<Cell[]>((result, xSplit, xIndex) => [...result, ...generate(xSplit, nxi => {
 		const table = cube[yIndex][xIndex];
 		const index = Math.floor(table.length * (nyi + nxi) / (xSplit * ySplit));
@@ -35,13 +45,6 @@ export function split<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>
 	})], yAxis[yIndex].data.map(pair => cell({ className: `axis y ${pair.key}`, text: pair.value }))))], generate(xAxis[0].data.length, row => xAxis.reduce<Cell[]>((res, measure, index) => [...res, ...generate(xSplits[index], () => cell({ className: `axis x ${measure.data[row].key}`, text: measure.data[row].value }))], yAxis[0].data.map(() => cell({ className: 'axis xy', text: '' })))));
 }
 
-/**
- * Creates a cell within a table
- * @hidden
- */
-function cell(key: Key): Cell {
-	return { ...key, rowSpan: 1, colSpan: 1 };
-}
 /**
  * Merge adjacent cells in a split table on the y and/or x axes.
  * @param table A table of Cells created by a previous call to splitX or splitY.
@@ -69,6 +72,20 @@ export function merge(table: Array<Array<Cell>>, onX = true, onY = true): void {
 }
 
 /**
+ * Create any array of numbers from 0 to n
+ * @hidden
+ */
+ function generate<TResult>(length: number, generator: Func1<number, TResult>): Array<TResult> {
+	const result = [];
+
+	for (let i = 0; i < length; i++) {
+		result.push(generator(i));
+	}
+
+	return result;
+}
+
+/**
  * Returns the least common multiple of two integers
  * @hidden
  */
@@ -85,15 +102,9 @@ function greatestCommonFactor(a: number, b: number): number {
 }
 
 /**
- * Create any array of numbers from 0 to n
+ * Creates a cell within a table, augmenting a key with row and column span detail
  * @hidden
  */
-function generate<TResult>(length: number, generator: Func1<number, TResult>): Array<TResult> {
-	const result = [];
-
-	for (let i = 0; i < length; i++) {
-		result.push(generator(i));
-	}
-
-	return result;
+ function cell(key: Key): Cell {
+	return { ...key, rowSpan: 1, colSpan: 1 };
 }
