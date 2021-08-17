60437aaf21caae5e355c0f1096ff6a0d28281d8e
David Mesquita-Morris <mesmo@steelbreeze.net>
2021-08-17 19:15:23 +0100

    Better structure within table function

--- src/index.ts
+++ src/index.ts
@@ -28,26 +28,30 @@ export interface Cell extends Key {
  * @param getKey A callback to generate a key containing the text and className used in the table from the source records,
  * @param onX A flag to indicate if cells in cube containing multiple values should be split on the x axis (if not, the y axis will be used).
  */
-export function table<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>, yAxis: Dimension<TRow>, getKey: Func1<TRow, Key>, onX: boolean): Array<Array<Cell>> {
+export function table<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>, yAxis: Dimension<TRow>, getKey: Func1<TRow, Key>, onX: boolean) {//}: Array<Array<Cell>> {
 	// for each row and column, determine how many sub rows and columns we need to split it into; this is the LCM of the counts of items in that row or column
 	const xSplits = generate(xAxis.length, index => onX ? cube.map(row => row[index].length || 1).reduce(leastCommonMultiple) : 1);
 	const ySplits = cube.map(row => row.map(table => onX ? 1 : table.length || 1).reduce(leastCommonMultiple));
 
-	// generate the table from the cube, split cell with more than one value by row or column based on the splits calculated
-	return expand(ySplits, (ySplit, yIndex) => generate(ySplit, nyi => expand(xSplits, (xSplit, xIndex) => generate(xSplit, nxi => {
-		const items = cube[yIndex][xIndex];
+	return reduce(ySplits, (ySplit, yIndex) => {
+		return generate(ySplit, nyi => {
+			return reduce(xSplits, (xSplit, xIndex) => {
+				const items = cube[yIndex][xIndex];
 
-		return cell(items.length ? getKey(items[Math.floor(items.length * (nyi + nxi) / (xSplit * ySplit))]) : { text: '', className: 'empty' });
-	}),
-
-		// generate the y axis (row) headings
-		yAxis[yIndex].data.map(pair => cell({ className: `axis y ${pair.key}`, text: pair.value })))),
-
-		// generate the x axis (column) headings
-		generate(xAxis[0].data.length, row => expand(xSplits, (xSplit, xIndex) => generate(xSplit, () => cell({ className: `axis x ${xAxis[xIndex].data[row].key}`, text: xAxis[xIndex].data[row].value })),
-
-			// generat the y/x header block
-			yAxis[0].data.map(() => cell({ className: 'axis xy', text: '' })))));
+				return generate(xSplit, nxi => {
+					return cell(items.length ? getKey(items[Math.floor(items.length * (nyi + nxi) / (xSplit * ySplit))]) : { text: '', className: 'empty' });
+				});
+			}, yAxis[yIndex].data.map(pair => {
+				return axis(pair, 'y');
+			}));
+		});
+	}, generate(xAxis[0].data.length, yIndex => {
+		return reduce(xSplits, (xSplit, xIndex) => {
+			return generate(xSplit, () => axis(xAxis[xIndex].data[yIndex], 'x'));
+		}, yAxis[0].data.map(() => {
+			return cell({ className: 'axis xy', text: '' });
+		}));
+	}));
 }
 
 /**
@@ -59,8 +63,12 @@ export function table<TRow extends Row>(cube: Cube<TRow>, xAxis: Dimension<TRow>
 export function merge(table: Array<Array<Cell>>, onX = true, onY = true): void {
 	let next;
 
-	forReverse(table, (row, iY) => {
-		forReverse(row, (cell, iX) => {
+	for (let iY = table.length; iY--;) {
+		const row = table[iY];
+
+		for (let iX = row.length; iX--;) {
+			const cell = row[iX];
+
 			if (onY && iY && (next = table[iY - 1][iX]) && next.text === cell.text && next.className === cell.className && next.colSpan === cell.colSpan) {
 				next.rowSpan += cell.rowSpan;
 
@@ -70,26 +78,21 @@ export function merge(table: Array<Array<Cell>>, onX = true, onY = true): void {
 
 				row.splice(iX, 1);
 			}
-		});
-	});
-}
-
-/**
- * Reverse for loop.
- * @hidden 
- */
-function forReverse<TSource>(source: Array<TSource>, callback: Func2<TSource, number, void>): void {
-	for (let index = source.length; index--;) {
-		callback(source[index], index);
+		}
 	}
 }
 
+//function expand<TSource, TResult>(source: Array<TSource>, f: (value: TSource, a: number, b: number) =)
+//return reduce(ySplits, (ySplit, yIndex) => {
+//	return generate(ySplit, nyi => {
+
+
 /**
- * Expand one array into another.
+ * Custom version of Array.prototype.reduce that adds arrays to the seed result.
  * @hidden 
  */
-function expand<TSource, TResult>(source: Array<TSource>, expander: Func2<TSource, number, Array<TResult>>, result: Array<TResult>): Array<TResult> {
-	source.forEach((value, index) => result.push(...expander(value, index)));
+function reduce<TSource, TResult>(source: Array<TSource>, expand: Func2<TSource, number, Array<TResult>>, result: Array<TResult>): Array<TResult> {
+	source.forEach((value, index) => result.push(...expand(value, index)));
 
 	return result;
 }
@@ -130,4 +133,12 @@ function greatestCommonFactor(a: number, b: number): number {
  */
 function cell(key: Key): Cell {
 	return { ...key, rowSpan: 1, colSpan: 1 };
+}
+
+/**
+ * Creates a cell within a table for a column or row heading.
+ * @hidden 
+ */
+function axis(pair: { key: string, value: string }, name: string): Cell {
+	return cell({ text: pair.value, className: `axis ${name} ${pair.key}` });
 }
